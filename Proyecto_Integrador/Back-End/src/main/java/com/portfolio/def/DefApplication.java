432ec4d29194b9215c8db2b884ff69538fba20e1
anothersonatine <demianemanuelflores@gmail.com>
2022-08-17 19:05:53 -0300

    back end

---
+++ Proyecto_Integrador/Back-End/src/main/java/com/portfolio/def/DefApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.def;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefApplication.class, args);
+	}
+
+}
---
+++ Proyecto_Integrador/Back-End/src/test/java/com/portfolio/def/DefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.def;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
