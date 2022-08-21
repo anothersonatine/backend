c6e467f6d7ddee20ce0a9c4307c6a2689615c1d7
anothersonatine <demianemanuelflores@gmail.com>
2022-08-21 01:05:43 -0300

    back end java

---
+++ Proyecto_Integrador/Back-End/src/main/java/com/portfolio/def/Repository/IPersonaRepository.java
@@ -0,0 +1,11 @@
+
+package com.portfolio.def.Repository;
+
+import com.portfolio.def.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona, Long> {
+    
+}
